dcd3ed1e7eec7182729076f64d121ed0452cebac
Joshua Wharton <joshuawharton1997@gmail.com>
2025-02-21 11:43:33 +0000

    Public feed example

--- examples/README.md
+++ examples/README.md
@@ -1,8 +1,14 @@
-# proto
+# Examples
 
-## Python Stubs
+# Setup protobufs
+Ensure you have `grpcio-tools` python package installed
 ```
 python -m grpc_tools.protoc -I. --python_out=./examples --grpc_python_out=./examples market_data.proto
 python -m grpc_tools.protoc -I. --python_out=./examples --grpc_python_out=./examples port.proto
 python -m grpc_tools.protoc -I. --python_out=./examples --grpc_python_out=./examples common.proto
+```
+
+# Run script
+```
+uv run examples/market_data_order_book.py
 ```
---
+++ examples/market_data_order_book.py
@@ -0,0 +1,37 @@
+# /// script
+# dependencies = [
+#   "grpcio-tools>=1.70.0",
+# ]
+# ///
+
+import grpc
+
+import common_pb2 as common_pb2
+import market_data_pb2 as market_data_pb2
+import market_data_pb2_grpc as market_data_pb2_grpc
+
+"""
+TO RUN THIS FILE:
+
+1. cd into parent directory
+2. uv run examples/market_data_order_book.py
+"""
+
+
+def run():
+    channel = grpc.insecure_channel("mds.psex.io:443")
+
+    creds = grpc.ssl_channel_credentials(
+        root_certificates=None, private_key=None, certificate_chain=None
+    )
+    channel = grpc.secure_channel("mds.psex.io:443", creds)
+
+    stub = market_data_pb2_grpc.MarketDataServiceStub(channel)
+
+    request = market_data_pb2.GetSymbol(symbol="AAPL")
+    for response in stub.GetOrderBook(request):
+        print(f"Received data: {response}")
+
+
+if __name__ == "__main__":
+    run()
